8a1566791536670d0f1e6ddb210f2c7c4806d232
Prateek Rath <prateekrath28@gmail.com>
2022-08-27 14:53:46 +0530

    redux news api

--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -23,20 +23,3 @@ export const {
     useGetCryptosQuery,
 }=cryptoApi
 
-// const options = {
-//     method: 'GET',
-//     url: 'https://coinranking1.p.rapidapi.com/coins',
-//     params: {
-//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
-//       timePeriod: '24h',
-//       'tiers[0]': '1',
-//       orderBy: 'marketCap',
-//       orderDirection: 'desc',
-//       limit: '50',
-//       offset: '0'
-//     },
-//     headers: {
-//       'X-RapidAPI-Key': 'c74669378amshebbecdcce37cb60p113532jsnf170b3929ff7',
-//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
-//     }
-//   };
