d45df7e6e99103a4b8b4275bfa28b78cb8351921
Prateek Rath <prateekrath28@gmail.com>
2022-08-27 18:55:33 +0530

    implement news search

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,16 +2,36 @@ import React from 'react'
 import {Select,Typography,Row,Col,Avatar,Card} from 'antd'
 import moment from 'moment'
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi'
+import { useGetCryptosQuery } from '../services/cryptoAPI'
+import { useState } from 'react'
 
 const {Text,Title}=Typography
 const { Option } = Select
 const demoImage='https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News'
 
-const News = ({simplified}) => {
-  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 6 : 12 })
+const News = ({ simplified }) => {
+  const [newsCategory,setNewsCategory]=useState('Cryptocurrency')
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: newsCategory, count: simplified ? 6 : 16 })
   console.log(cryptoNews)
+  const { data} = useGetCryptosQuery(100)
   if(!cryptoNews?.value)return 'Loading...'
   return (
+  <>
+    {!simplified && (
+        <Col span={24} className='news-search'>
+          <Select
+            showSearch
+            className='select-news'
+            placeholder='Select a Crypto'
+            optionFilterProp='children'
+            onChange={(value) => setNewsCategory(value)}
+            filterOption={(input,option)=>option.children.toLowerCase().indexOf(input.toLowerCase())>=0}
+          >
+            <Option value="Cryptocurrency">Cryptocurrency</Option>
+            {data?.data?.coins.map((coin) => <Option value={coin.name} key={ coin.uuid}>{ coin.name}</Option>)}
+          </Select>
+      </Col>
+    )}
     <Row gutter={[24, 24]}>
       {cryptoNews.value.map((news,i) => (
         <Col xs={24} sm={12} lg={8} key={i}>
@@ -19,7 +39,7 @@ const News = ({simplified}) => {
             <a href={news.url} target='_blank' rel="noreferrer">
               <div className="news-image-container">
                 <Title className='news-title' level={4}>{news.name}</Title>
-                <img src={news?.image?.thumbnail?.contentUrl || demoImage} alt="news" />
+                <img style={{maxWidth:'200px',maxHeight:'100px'}} src={news?.image?.thumbnail?.contentUrl || demoImage} alt="news" />
               </div>
               <p>
                 {news.description>100?`${news.description.substring(0.100)}...`:news.description}
@@ -36,7 +56,8 @@ const News = ({simplified}) => {
           </Card>
         </Col>
       ))}
-    </Row>
+      </Row>
+      </>
   )
 }
 
